c88ab5bc36fb854e193b322e85559d2b52067a55
DaeHee <pulsmell@gmail.com>
2020-10-05 13:23:07 +0900

    calander

---
+++ Keith-bigdata/src/calander.java
@@ -0,0 +1,29 @@
+
+public class calander {
+
+	public static void main(String[] args) {
+	
+		int Month = 1;
+		
+		
+		switch(Month){
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+			System.out.println("31일 입니다.");
+			break;
+		case 2:
+			System.out.println("31일 입니다.");
+			break;
+		case 4:
+		case 6:
+		case 9:
+			System.out.println("30일 입니다.");
+			break;
+		
+	}
+	
+ }
+}
